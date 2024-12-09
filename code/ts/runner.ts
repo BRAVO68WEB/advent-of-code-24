cf391d37e13da1b1445d2eed902be5da9d485785
bravo68web <jbandyopadhayaya@gmail.com>
2024-12-09 04:40:15 +0000

    chore: minor patch fixes

--- code/ts/runner.ts
+++ code/ts/runner.ts
@@ -1,39 +1,39 @@
-// import { partone as d1p1, parttwo as d1p2 } from "./01/code";
-// import { partone as d2p1, parttwo as d2p2 } from "./02/code";
-// import { partone as d3p1, parttwo as d3p2 } from "./03/code";
-// import { partone as d4p1, parttwo as d4p2 } from "./04/code";
-// import { partone as d5p1, parttwo as d5p2 } from "./05/code";
-// import { partone as d6p1, parttwo as d6p2 } from "./06/code";
-// import { partone as d7p1, parttwo as d7p2 } from "./07/code";
+import { partone as d1p1, parttwo as d1p2 } from "./01/code";
+import { partone as d2p1, parttwo as d2p2 } from "./02/code";
+import { partone as d3p1, parttwo as d3p2 } from "./03/code";
+import { partone as d4p1, parttwo as d4p2 } from "./04/code";
+import { partone as d5p1, parttwo as d5p2 } from "./05/code";
+import { partone as d6p1, parttwo as d6p2 } from "./06/code";
+import { partone as d7p1, parttwo as d7p2 } from "./07/code";
 import { partone as d8p1, parttwo as d8p2 } from "./08/code";
 
-// console.log(`-- Day 1 --`);
-// console.log(`Part 1: ${d1p1}`);
-// console.log(`Part 2: ${d1p2}`);
+console.log(`-- Day 1 --`);
+console.log(`Part 1: ${d1p1}`);
+console.log(`Part 2: ${d1p2}`);
 
-// console.log(`-- Day 2 --`);
-// console.log(`Part 1: ${d2p1}`);
-// console.log(`Part 2: ${d2p2}`);
+console.log(`-- Day 2 --`);
+console.log(`Part 1: ${d2p1}`);
+console.log(`Part 2: ${d2p2}`);
 
-// console.log(`-- Day 3 --`);
-// console.log(`Part 1: ${d3p1}`);
-// console.log(`Part 2: ${d3p2}`);
+console.log(`-- Day 3 --`);
+console.log(`Part 1: ${d3p1}`);
+console.log(`Part 2: ${d3p2}`);
 
-// console.log(`-- Day 4 --`);
-// console.log(`Part 1: ${d4p1}`);
-// console.log(`Part 2: ${d4p2}`);
+console.log(`-- Day 4 --`);
+console.log(`Part 1: ${d4p1}`);
+console.log(`Part 2: ${d4p2}`);
 
-// console.log(`-- Day 5 --`);
-// console.log(`Part 1: ${d5p1}`);
-// console.log(`Part 2: ${d5p2}`);
+console.log(`-- Day 5 --`);
+console.log(`Part 1: ${d5p1}`);
+console.log(`Part 2: ${d5p2}`);
 
-// console.log(`-- Day 6 --`);
-// console.log(`Part 1: ${d6p1}`);
-// console.log(`Part 2: ${d6p2}`);
+console.log(`-- Day 6 --`);
+console.log(`Part 1: ${d6p1}`);
+console.log(`Part 2: ${d6p2}`);
 
-// console.log(`-- Day 7 --`);
-// console.log(`Part 1: ${d7p1}`);
-// console.log(`Part 2: ${d7p2}`);
+console.log(`-- Day 7 --`);
+console.log(`Part 1: ${d7p1}`);
+console.log(`Part 2: ${d7p2}`);
 
 console.log(`-- Day 8 --`);
 console.log(`Part 1: ${d8p1}`);
